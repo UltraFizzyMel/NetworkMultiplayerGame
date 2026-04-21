624032d95b4a0ac69d2a785beb81e22d4bb0a758
UltraFizzyMel <2344445@students.wits.ac.za>
2026-04-21 11:56:13 +0200

    Lobby Attempt One

--- NetworkMultiplayer/Assets/_Scripts/TestingLobbyUI.cs
+++ NetworkMultiplayer/Assets/_Scripts/TestingLobbyUI.cs
@@ -1,8 +1,41 @@
 using UnityEngine;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
 using UnityEngine.UI;
 
 public class TestingLobbyUI : MonoBehaviour
 {
-    [SerializeField] private Button btncreateGame;
-    [SerializeField] private Button btnjoinGame;
+    //[SerializeField] private Button btncreateGame;
+    //[SerializeField] private Button btnjoinGame;
+
+    private async void Start()
+    {
+        await UnityServices.InitializeAsync();
+
+        AuthenticationService.Instance.SignedIn += () =>
+        {
+            Debug.Log("Signed in anonymously with player ID: " + AuthenticationService.Instance.PlayerId);
+        };
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+
+    private async void CreateLobby()
+    {
+        try {
+            string LobbyName = "MyLobby";
+            int maxPlayers = 2;
+            Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(LobbyName, maxPlayers);
+
+            Debug.Log("Lobby created with ID: " + lobby.Id);
+        } catch (LobbyServiceException e) {
+            Debug.LogError("Failed to create lobby: " + e.Message);
+        }
+    }
+
+    private async void ListLobbies()
+    {
+        //await Lobbies.Instance.QueryLobbiesAsync();
+    }
 }
