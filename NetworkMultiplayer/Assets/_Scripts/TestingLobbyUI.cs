9e2bc76cc957791fac74fc0e9e5d916a7c6c1b9c
UltraFizzyMel <2344445@students.wits.ac.za>
2026-04-26 10:17:08 +0200

    Small fix

--- NetworkMultiplayer/Assets/_Scripts/TestingLobbyUI.cs
+++ NetworkMultiplayer/Assets/_Scripts/TestingLobbyUI.cs
@@ -4,11 +4,15 @@ using Unity.Services.Authentication;
 using Unity.Services.Lobbies;
 using Unity.Services.Lobbies.Models;
 using UnityEngine.UI;
+using System.Threading.Tasks;
 
 public class TestingLobbyUI : MonoBehaviour
 {
     //[SerializeField] private Button btncreateGame;
     //[SerializeField] private Button btnjoinGame;
+
+    private Lobby hostLobby;
+    private float heartbeatTimer;
 
     private async void Start()
     {
@@ -21,12 +25,41 @@ public class TestingLobbyUI : MonoBehaviour
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
     }
 
+    private void Update()
+    {
+        if (hostLobby != null)
+        {
+            heartbeatTimer -= Time.deltaTime;
+            if (heartbeatTimer <= 0f)
+            {
+                heartbeatTimer = 15f;
+                SendHeartbeat();
+            }
+        }
+    }
+
+    private void HandleLobbyHeartbeat()
+    {
+        heartbeatTimer = 15f;
+    }
+
+    private async Task SendHeartbeat()
+    {
+        if (hostLobby != null)
+        {
+            await LobbyService.Instance.SendHeartbeatPingAsync(hostLobby.Id);
+            Debug.Log("Sent heartbeat ping for lobby ID: " + hostLobby.Id);
+        }
+    }
+
     private async void CreateLobby()
     {
         try {
             string LobbyName = "MyLobby";
             int maxPlayers = 2;
             Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(LobbyName, maxPlayers);
+
+            hostLobby = lobby;
 
             Debug.Log("Lobby created with ID: " + lobby.Id);
         } catch (LobbyServiceException e) {
@@ -36,6 +69,19 @@ public class TestingLobbyUI : MonoBehaviour
 
     private async void ListLobbies()
     {
-        //await Lobbies.Instance.QueryLobbiesAsync();
+        try
+        {
+            QueryResponse queryResponse = await LobbyService.Instance.QueryLobbiesAsync();
+
+            Debug.Log("Lobbies found: " + queryResponse.Results.Count);
+            foreach (var lobby in queryResponse.Results)
+            {
+                Debug.Log(lobby.Name + " " + lobby.MaxPlayers);
+            }
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.LogError("Failed to list lobbies: " + e.Message);
+        }
     }
 }
