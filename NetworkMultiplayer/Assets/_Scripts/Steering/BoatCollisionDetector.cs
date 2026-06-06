51080256d97c0af5136e5c6fcf256b152ddc7228
Lu-Wannell <2727686@students.wits.ac.za>
2026-06-06 16:19:33 +0200

    Update BoatCollisionDetector.cs

--- NetworkMultiplayer/Assets/_Scripts/Steering/BoatCollisionDetector.cs
+++ NetworkMultiplayer/Assets/_Scripts/Steering/BoatCollisionDetector.cs
@@ -72,13 +72,9 @@ public class BoatCollisionDetector : NetworkBehaviour
             StartCoroutine(CollisionCooldownRoutine());
         }
 
-        //Destroy rock after collision
-        NetworkObject no = rock.GetComponent<NetworkObject>();
+       
+        RequestDestroyServerRpc(rock);
 
-        if (no != null && no.IsSpawned)
-            no.Despawn();
-        else
-            Destroy(rock.gameObject);
     }
 
     private IEnumerator CollisionCooldownRoutine()
@@ -96,5 +92,28 @@ public class BoatCollisionDetector : NetworkBehaviour
             player.StartCameraShake(intensity, duration);
             break;
         }
+    }
+
+    [Rpc(SendTo.Server, InvokePermission = RpcInvokePermission.Everyone)]
+    public void RequestDestroyServerRpc(RockObstacle rock)
+    {
+        RequestDestroyClientRpc(rock);
+    }
+
+    [ClientRpc]
+    public void RequestDestroyClientRpc(RockObstacle rock)
+    {
+        DestroySelf(rock);
+    }
+
+    public void DestroySelf(RockObstacle rock)
+    {
+        //Destroy rock after collision
+        NetworkObject no = rock.GetComponent<NetworkObject>();
+
+        if (no != null && no.IsSpawned)
+            no.Despawn();
+        else
+            Destroy(rock.gameObject);
     }
 }
